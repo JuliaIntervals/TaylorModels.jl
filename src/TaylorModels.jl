ff92ad681acafbbeab7efb1a3236e51891bddd99
David Sanders <dpsanders@gmail.com>
2017-11-19 16:35:27 -0600

    Move bounding functions to bound.jl

--- src/TaylorModels.jl
+++ src/TaylorModels.jl
@@ -41,59 +41,12 @@ end
 
 include("arithmetic.jl")
 include("functions.jl")
+include("bound.jl")
 
 include("integrate.jl")
 include("draw.jl")
 
 
-
-doc"""
-Compute a rigorous bound for a TaylorModel.
-"""
-function bound(f::TaylorModel)
-    x0, I, p = f.x0, f.I, f.p
-
-    B = zero(I)
-    n = degree(p)
-
-    for i = n:-1:0
-        B = B * (I - x0) + bound(p[i], f.bounds)
-    end
-
-    return B
-end
-
-function bound(p::Taylor1, x0, I)
-
-    B = zero(I)
-    n = degree(p)
-
-    for i = n:-1:0
-        B = B * (I - x0) + p[i]
-    end
-
-    return B
-end
-
-function bound(p::Taylor1, x0, I, bounds)
-
-    B = zero(I)
-    n = degree(p)
-
-    for i = n:-1:0
-        B = B * (I - x0) + bound(p[i], bounds)
-    end
-
-    return B
-end
-
-
-bound(f::TaylorN, bounds) = evaluate(f, bounds)  # can replace by better polynomial bounder
-
-
-# bound(f::TaylorModel) = bound(f.p, f.x0, f.I)
-
-bound(x::Interval, bounds) = x
 
 # TaylorModel for a constant:
 # TaylorModel(n::Int, x0, I, c::T) where {T<:AbstractFloat} = TaylorModel{T}(n, x0, I, Taylor1{Interval{T}}(c), Interval{T}(0), [])
