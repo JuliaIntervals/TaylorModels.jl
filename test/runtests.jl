0bc97ffed56297ac0250f6c4df5fd5f53b2fbae3
David Sanders <dpsanders@gmail.com>
2018-02-07 13:53:44 -0600

    First tests for Taylor1

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,33 @@
-using TaylorModels
+using Base.Test, TaylorModels, IntervalArithmetic
 
-import Documenter
-Documenter.makedocs(
-    modules = [TaylorModels],
-    format = :html,
-    sitename = "TaylorModels.jl",
-    root = joinpath(dirname(dirname(@__FILE__)), "docs"),
-    pages = Any["Home" => "index.md"],
-    strict = true,
-    linkcheck = true,
-    checkdocs = :exports,
-    authors = "David Sanders"
-)
-
-using Base.Test
+# import Documenter
+# Documenter.makedocs(
+#     modules = [TaylorModels],
+#     format = :html,
+#     sitename = "TaylorModels.jl",
+#     root = joinpath(dirname(dirname(@__FILE__)), "docs"),
+#     pages = Any["Home" => "index.md"],
+#     strict = true,
+#     linkcheck = true,
+#     checkdocs = :exports,
+#     authors = "David Sanders"
+# )
 
 # write your own tests here
-@test 1 == 2
+
+@testset "Constructor" begin
+    t = taylor1_var(3, 0, -0.5..0.5)
+
+    @test t isa Taylor1Model
+    @test t.n == 3
+    @test t.x0 == 0
+    @test t.I == -0.5..0.5
+end
+
+@testset "Exponential" begin
+    t = taylor1_var(3, 0, -0.5..0.5)
+    s = exp(t)
+
+    @test s isa Taylor1Model
+
+end
