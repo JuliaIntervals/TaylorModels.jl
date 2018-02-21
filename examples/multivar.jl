111f2eba91f38f5e28052643e66afcd54fefdcac
David Sanders <dpsanders@gmail.com>
2018-02-21 17:28:48 -0600

    Integrate for TaylorN; update multivar.jl

--- examples/multivar.jl
+++ examples/multivar.jl
@@ -1,63 +1,67 @@
 using TaylorModels, IntervalArithmetic, TaylorSeries
 
-a, b = set_variables("a b", order=3)
+t, a, b = set_variables(Interval{Float64}, "t a b", order=3)
 
-u0 = Taylor1([1 + a], 3)
+u00 = 1+a   # initial condition
 
-t = TaylorModel(3, 0..0, -0.1..0.1, u0, 0..0, [-0.05..0.05, -0.05..05])
-
-bound(t)
+II = -0.1..0.1
+u0 = TaylorNModel(3, IntervalBox(0..0, 0..0, 0..0), IntervalBox(II, II, II), u00, 0..0)
 
 
 # SOLVE ODE  ẋ = x:
-
-using TaylorModels, IntervalArithmetic, TaylorSeries
-
-n = 4  # order
-a, b = set_variables("a b", order=n)
-bounds = [-0.05..0.05, -0.05..0.05]  # bounds on a and b
-
-u0 = Taylor1([1 + a], n)   # initial condition as function of a, b
-
-# Find (non-rigorous) Taylor series expansion by Picard iteration:
+n = 3
 ∫ = integrate
 u = u0
-for i in 1:n+1   # how many iterations are required?
-    u = u0 + ∫(u)   # solving ẋ = x
+for i in 1:n+5   # how many iterations are required?
+    u = u0 + ∫(u, 1)
 end
 
-@assert u == u0 + ∫(u)   # fixed point of non-rigorous Picard
+u
+
+# u == u0 + ∫(u,1)   # fixed point of non-rigorous Picard
+# need to define equality
+
+integrate(u, 1)
 
 t_interval = 0..0.1
 
 # Prepare a TaylorModel with 0 error term to estimate size of required interval
-t = TaylorModel(n, 0..0, t_interval, u, 0..0, bounds)
+w = TaylorNModel(u.n, u.x0, u.I, u.p, 0..0)
 
-t_new = ∫(t)  # just so that t_new exists outside the loop
-
+w_new = ∫(w, 1)  # just so that t_new exists outside the loop
 
 # Search for interval giving contraction, and hence rigorous Picard iteration
 
 for i in 1:10
-    t_new = ∫(t, u0[0])
-    @show t.Δ, t_new.Δ, t_new.Δ ⊆ t.Δ
+    w_new = u0 + ∫(w, 1)
+    @show w.Δ, w_new.Δ, w_new.Δ ⊆ t.Δ
 
-    (t_new.Δ ⊆ t.Δ) && break  # if this happens, then have found contraction
+    (w_new.Δ ⊆ w.Δ) && break  # if this happens, then have found contraction
 
-    Δ = 2*t_new.Δ  # otherwise keep increasing the size of the error bound interval
-    t = TaylorModel(n, 0..0, t_interval, u, Δ, bounds)
+    Δ = 2*w_new.Δ  # otherwise keep increasing the size of the error bound interval
+    w = TaylorNModel(w.n, w.x0, w.I, w.p, Δ)
 end
 
+w
 
-# Iterate contraction until reach fixed point:
+w.p
+
+evaluate # Iterate contraction until reach fixed point:
 
 for i in 1:20
-    t_new = ∫(t, u0[0])
-    @show t.Δ, t_new.Δ, t_new.Δ ⊆ t.Δ, t_new.Δ == t.Δ
+    w_new = u0 + ∫(w, 1)
+    @show w.Δ, w_new.Δ, w_new.Δ ⊆ w.Δ, w_new.Δ == w.Δ
 
-    Δ = t_new.Δ
-    t = TaylorModel(n, 0..0, t_interval, u, Δ, bounds)
+    Δ = w_new.Δ
+    w = TaylorNModel(w.n, w.x0, w.I, w.p, Δ)
 end
+
+w
+
+w.p([t_interval.hi..t_interval.hi, a, b]) + w.Δ
+
+(1+a)*exp(0.1..0.1)
+
 
 # Find initial set for next step:
 tf = (t_interval.hi)..(t_interval.hi)
