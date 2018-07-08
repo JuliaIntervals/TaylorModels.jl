00690e9f7ad6cefa0a9cd4cab4e5f7ac36843398
Luis Benet <benet@fis.unam.mx>
2018-07-08 00:00:12 -0500

    Export polynomial

--- src/TaylorModels.jl
+++ src/TaylorModels.jl
@@ -24,7 +24,7 @@ export Taylor1Model, bound, make_Taylor_model, TMcomposition,
         calculate_set, Taylor_step
 
 export TM1AbsRem, TM1RelRem, TMNAbsRem,
-    remainder,
+    remainder, polynomial, 
     rpa, fp_rpa, boundarem, boundrrem
 
 
