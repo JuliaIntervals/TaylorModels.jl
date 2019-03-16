9c733fd11fc171101905a7dd2237f2594c5642d7
Arpit Bhatia <arpit16229@iiitd.ac.in>
2019-03-16 13:43:30 +0530

    Add introductory docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,14 @@
-import Documenter
+using Documenter, TaylorModels
+
+makedocs(
+    modules = [TaylorModels],
+    format = Documenter.HTML(),
+    sitename = "TaylorModels",
+    pages = [
+        "Home" => "index.md",
+        "API" => "api.md"
+    ]
+)
 
 Documenter.deploydocs(
     repo = "github.com/dpsanders/TaylorModels.jl.git",
