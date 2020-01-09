274cdb9e75fc5d44995278bdc1f0d2a62e167ab8
Luis Benet <benet@icf.unam.mx>
2020-01-08 22:01:21 -0600

    Fix picard_lindelof and clean-up integration.jl

--- src/integration.jl
+++ src/integration.jl
@@ -57,7 +57,7 @@ This is tighter that the one used by Berz+Makino, which corresponds to
 function bound_integration(a::TaylorModel1{T,S}, δ) where {T,S}
     order = get_order(a)
     aux = δ^order / (order+1)
-    Δ = δ * (remainder(a) + getcoeff(polynomial(a),order) * aux)
+    Δ = δ * (remainder(a) + getcoeff(polynomial(a), order) * aux)
     return Δ
 end
 function bound_integration(a::Vector{TaylorModel1{T,S}}, δ) where {T,S}
@@ -69,114 +69,23 @@ end
 
 
 
-"""
-    picard_lindelöf(f, tm::T, xm::T, x0::Interval)
-    𝒫(f, tm::T, xm::T, x0::Interval)
+function picard_lindelof(f!, dxTM1TMN::Vector{TaylorModel1{T,S}},
+        xTM1TMN::Vector{TaylorModel1{T,S}}, t, params) where {T,S}
 
-Returns the application of the Picard-Lindelöf operator
-associated to the ODE ``\\dot{x} = f(t,x)``,
-with initial condition `x0`. Here, `tm` and `xm` are
-(one-variable) Taylor Models (`TaylorModel1` or `RTaylorModel1`).
-
-𝒫 is an abbreviation of this operator, which is obtained
-as `\\mscrP<TAB>`.)
-"""
-picard_lindelöf(f, tm::T, xm::T, x0::Interval) where
-    {T<:Union{TaylorModel1, RTaylorModel1}} = integrate(f(tm, xm), x0)
-
-const 𝒫 = picard_lindelöf
-
-"""
-    check_existence(f, tm::T, xm::T, x0::Interval, x_test::Interval)
-
-Checks that the range of one iterate of the Picard-Lindelöf
-operator is contained in the a-priori interval `x_test` (of
-the dependent variable) that bounds the solution of
-the ODE defined by `f`. This function returns an interval
-of the independent variable where the a-priori solution
-is warranted to exist; see [`shrink_for_existance`](@ref).
-Here, `tm` and `xm` are Taylor Models (`TaylorModel1` or `RTaylorModel1`)
-defined for the independent and dependent variables, and `x0`
-is the initial condition.
-"""
-function check_existence(f, tm::T, xm::T, x0::Interval, x_test::Interval,
-        max_steps::Integer=20) where {T<:Union{TaylorModel1, RTaylorModel1}}
-
-    pl = 𝒫(f, tm, xm, x0)
-    tt = shrink_for_existance(pl, tm.dom, x_test, max_steps)
-    if pl(tt-tm.x0) ⊆ x_test
-        return tt
-    else
-        return emptyinterval(tt)
-    end
+    x_picard = similar(xTM1TMN)
+    picard_lindelof!(f!, dxTM1TMN, xTM1TMN, t, x_picard, params)
+    return x_picard
 end
 
-"""
-    shrink_for_existance(xm::T, t_interval, x_test, max_steps::Integer=20)
+function picard_lindelof!(f!,
+        dxTM1TMN::Vector{TaylorModel1{T,S}},
+        xTM1TMN::Vector{TaylorModel1{T,S}},
+        x_picard::Vector{TaylorModel1{T,S}}, t, params) where {T,S}
 
-Shrinks the a-priori independent-variable interval `t_interval`
-so the range of `xm`, the Taylor Model (`TaylorModel1` or `RTaylorModel1`)
-associated with the dependent variable, is contained in the
-a-priori interval `x_test`. The method used is some sort
-of bisection. If no independent-variable interval is found within
-`max_steps`, an empty interval is returned.
-"""
-function shrink_for_existance(xm::T, t_interval, x_test, max_steps::Integer=20) where
-        {T<:Union{TaylorModel1, RTaylorModel1}}
-
-    tt = t_interval
-    t1 = t_interval
-
-    nsteps = 0
-    xm_pol = xm.pol
-
-    while nsteps < max_steps
-        if xm(t1) ⊆ x_test
-            (t_interval.hi-t1.hi < 1.0e-3) && return t1
-            tt = t1
-            mm = (tt.hi+t_interval.hi)*0.5
-            t1 = interval(t1.lo, mm)
-        else
-            t_interval = t1
-            if t1 == tt
-                mm = (tt.lo+t1.hi)*0.5
-                t1 = interval(t1.lo, mm)
-                tt = t1
-            else
-                mm = (tt.hi+t1.hi)*0.5
-                t1 = interval(t1.lo, mm)
-            end
-        end
-        nsteps += 1
+    dof = length(xTM1TMN)
+    f!(dxTM1TMN, xTM1TMN, params, t)
+    @inbounds for ind = 1:dof
+        x_picard[ind] = integrate(dxTM1TMN[ind], xTM1TMN[ind][0])
     end
-
-    xm(t1) ⊆ x_test && return t1
-    xm(tt) ⊆ x_test && return tt
-
-    return emptyinterval(tt)
-end
-
-"""
-    tight_remainder(f, tm::T, xm::T, x0::Interval, max_steps::Integer=20)
-
-Returns a Taylor Model for the dependent variable, with a tighter
-remainder, which is obtained after successive iteration
-of the Picard-Lindelöf. If the remainder is not tighter
-(and identity with the former iterate is not obtained) a
-Taylor Model with an empty interval is returned.
-
-"""
-function tight_remainder(f, tm::T, xm::T, x0::Interval, max_steps::Integer=20) where
-        {T<:Union{TaylorModel1, RTaylorModel1}}
-
-    xOld = deepcopy(xm)
-    for ind = 1:max_steps
-        xNew = 𝒫(f, tm, xOld, x0)
-        if diam(remainder(xNew)) ≥ diam(remainder(xOld))
-            xOld == xNew && return xOld
-            return T(xOld.pol, emptyinterval(xOld.rem), xOld.x0, xOld.dom)
-        end
-        xOld = xNew
-    end
-    return xOld
+    return x_picard
 end
