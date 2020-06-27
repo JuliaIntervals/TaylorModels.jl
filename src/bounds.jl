e36c4f6634471c0d1616699c7b458b8f9ecf1ea2
Uziel Linares <uzielnmtz@gmail.com>
2020-06-27 16:06:28 -0500

    add docstring for quadratic_fast_bounder

--- src/bounds.jl
+++ src/bounds.jl
@@ -253,11 +253,11 @@ function linear_dominated_bounder(fT::TaylorModelN{N, T, S}; ϵ=1e-5, max_iter=5
 end
 
 """
-    quadratic_fast_bounder(fT::TaylorModel)
+    quadratic_fast_bounder(fT::TaylorModel1)
 
 Compute a *tighter* polynomial bound by the quadratic fast bounder.
 The returned bound corresponds to the "improved" polynomial bound
-with the remainder of the `TaylorModel` included. This "improved" bound
+with the remainder of the `TaylorModel1` included. This "improved" bound
 can be one of the following two:
     1) An improved bound: if the domain of `fT` has a local minimizer,
        then an improved bound is returned.
@@ -285,6 +285,22 @@ function quadratic_fast_bounder(fT::TaylorModel1)
     end
 end
 
+"""
+    quadratic_fast_bounder(fT::TaylorModelN)
+
+Compute a *tighter* polynomial bound by the quadratic fast bounder.
+The returned bound corresponds to the "improved" polynomial bound
+with the remainder of the `TaylorModelN` included. This "improved" bound
+can be one of the following two:
+    1) An improved bound: if the domain of `fT` has a local minimizer,
+       then an improved bound is returned.
+    2) Original TaylorModel bound: if the local minimizer criteria is not
+       satisfied, then the original bound of `fT` is returned.
+
+This algorithm is a slight modification to the Makino & Berz algorithm.
+For this algorithm the linear part is bounded by solving a simple
+set of linear equations (compared to the iterative procedure by Makino & Berz).
+"""
 function quadratic_fast_bounder(fT::TaylorModelN)
     P = fT.pol
     H = Matrix(TaylorSeries.hessian(P))
