c60f416ed973dfc0ebd7c637b5c886c563a6b950
Uziel Linares <uzielnmtz@gmail.com>
2020-06-27 14:56:58 -0500

    add docstring for linear_dominated_bounder

--- src/bounds.jl
+++ src/bounds.jl
@@ -148,13 +148,13 @@ a definite sign.
 bound_taylor1(fT::TaylorModel1, I=domain(fT)::Interval) = bound_taylor1(polynomial(fT), I)
 
 """
-    linear_dominated_bounder(fT::TaylorModel1, ϵ=1e-3::Float, max_iter=5::Int)
+    linear_dominated_bounder(fT::TaylorModel1, ϵ=1e-3::Float64, max_iter=5::Int)
 
 Compute a tighter polynomial bound for the Taylor model `fT` by the linear
 dominated bounder algorithm. The linear dominated algorithm is applied until
 the bound of `fT` gets tighter than `ϵ` or the number of steps reachs `max_iter`.
 The returned bound corresponds to the improved polynomial bound with the remainder
-of the `TaylorModel` included.
+of the `TaylorModel1` included.
 """
 function linear_dominated_bounder(fT::TaylorModel1{S, T}; ϵ=1e-3, max_iter=5) where {S, T}
     d = 1.
@@ -199,6 +199,15 @@ function linear_dominated_bounder(fT::TaylorModel1{S, T}; ϵ=1e-3, max_iter=5) w
     return interval(bound.lo, hi) + fT.rem
 end
 
+"""
+    linear_dominated_bounder(fT::TaylorModelN, ϵ=1e-3::Float64, max_iter=5::Int)
+
+Compute a tighter polynomial bound for the Taylor model `fT` by the linear
+dominated bounder algorithm. The linear dominated algorithm is applied until
+the bound of `fT` gets tighter than `ϵ` or the number of steps reachs `max_iter`.
+The returned bound corresponds to the improved polynomial bound with the remainder
+of the `TaylorModelN` included.
+"""
 function linear_dominated_bounder(fT::TaylorModelN{R, S, T}; ϵ=1e-5, max_iter=5) where {R, S, T}
     d = 1.
     Dn = fT.dom
