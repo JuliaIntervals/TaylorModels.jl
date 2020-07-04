aab6bde91a18f1f350995f36ee8d1c0808ac3961
Uziel Linares <uzielnmtz@gmail.com>
2020-07-03 23:07:55 -0500

    add bound_integration for TMN and a new method for integrate

--- src/integration.jl
+++ src/integration.jl
@@ -43,12 +43,16 @@ function integrate(a::TaylorModel1{TaylorModelN{N,T,S},S},
     return TaylorModel1( integ_pol, ΔN, a.x0, a.dom )
 end
 
-function integrate(fT::TaylorModelN, which=1, x0=0)
+function integrate(fT::TaylorModelN, which=1, x0=0.)
     p̂ = integrate(fT.pol, which, x0)
     r = TaylorN(p̂.coeffs[1:fT.pol.order+1])
     s = TaylorN(p̂.coeffs[fT.pol.order+2:end])
-    Δ = s(fT.dom - fT.x0) + fT.rem * (fT.dom[which] - fT.x0[which])
+    Δ = bound_integration(fT, s, which)
     return TaylorModelN(r, Δ, fT.x0, fT.dom)
+end
+function integrate(fT::TaylorModelN, s::Symbol, x0=0.)
+    which = TaylorSeries.lookupvar(s)
+    return integrate(fT, which, x0)
 end
 
 """
@@ -73,11 +77,10 @@ function bound_integration(a::Vector{TaylorModel1{T,S}}, δ) where {T,S}
     Δ = δ .* (remainder.(a) .+ getcoeff.(polynomial.(a), order) .* aux)
     return IntervalBox(Δ)
 end
-function bound_integration(fT::TaylorModelN)
-
+function bound_integration(fT::TaylorModelN, s::TaylorN, which)
+    Δ = s(fT.dom - fT.x0) + fT.rem * (fT.dom[which] - fT.x0[which])
+    return Δ
 end
-
-
 
 function picard_lindelof(f!, dxTM1TMN::Vector{TaylorModel1{T,S}},
         xTM1TMN::Vector{TaylorModel1{T,S}}, t, params) where {T,S}
