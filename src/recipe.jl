08e5cbba801c50a4d3c0102717121210918d0f23
Luis Benet <benet@icf.unam.mx>
2021-05-26 19:11:57 -0500

    Add recipe for plotting TMSol structs

--- src/recipe.jl
+++ src/recipe.jl
@@ -44,3 +44,62 @@ end
 
     xs, ys
 end
+
+@recipe function g(sol::TMSol; vars=(0,1), ntdiv=1)
+    # Some initial checks
+    @assert ntdiv > 0 "`ntdiv must be 1 or larger"
+    @assert length(vars) == length(unique(vars)) == 2 "`vars` must have 2 distinct integer indices"
+    dof = get_numvars()
+    @assert 0 ≤ minimum(vars) && maximum(vars) ≤ dof
+
+    return _plot_intvbox(sol, vars=vars, ntdiv=ntdiv)
+end
+
+function _plot_intvbox(sol; vars=(0,1), ntdiv=1)
+    # Initializations
+    normalized_box = symmetric_box(2, Float64)
+    tTM = getfield(sol, :time)
+    xTMv = getfield(sol, :xTMv)
+    ltime = lastindex(tTM)
+    plotbox = Vector{IntervalBox{2, Float64}}(undef, ntdiv*(ltime-1))
+    v1 = Vector{Interval{Float64}}(undef, length(plotbox))
+    v2 = similar(v1)
+
+    # Case in which time is plotted
+    if 0 ∈ vars
+        tupT = findfirst(0 ∈ vars)
+        tup2 = vars[3-tupT]
+        ii = 1
+        @inbounds for indT in 2:ltime
+            domTv = mince(domain(xTMv[tup2,indT]), ntdiv)
+            for domT in domTv
+                v1[ii] = tTM[indT]+domT
+                v2[ii] = evaluate(xTMv[tup2,indT](domT), normalized_box)
+                ii += 1
+            end
+        end
+
+        if vars[1] == 0
+            @. plotbox = IntervalBox(v1, v2)
+        else
+            @. plotbox = IntervalBox(v2, v1)
+        end
+    else
+
+        # Case in which time is not plotted
+        var1 = vars[1]
+        var2 = vars[2]
+        ii = 1
+        @inbounds for indT in 2:ltime
+            domTv = mince(domain(xTMv[var1,indT]), ntdiv)
+            for domT in domTv
+                v1[ii] = evaluate(xTMv[var1,indT](domT), normalized_box)
+                v2[ii] = evaluate(xTMv[var2,indT](domT), normalized_box)
+                ii += 1
+            end
+        end
+        @. plotbox = IntervalBox(v1, v2)
+    end
+
+    return plotbox
+end
