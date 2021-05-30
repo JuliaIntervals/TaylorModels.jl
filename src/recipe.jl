ad0d6297f1c0693430d88bd5b622215c58ce5c42
Luis Benet <benet@icf.unam.mx>
2021-05-30 16:38:26 -0500

    Improve recipe, adding methods to mince in time

--- src/recipe.jl
+++ src/recipe.jl
@@ -45,60 +45,88 @@ end
     xs, ys
 end
 
-@recipe function g(sol::TMSol; vars=(0,1), ntdiv=1)
+@recipe function g(sol::TMSol; vars=(0,1), timediv=1)
     # Some initial checks
-    @assert ntdiv > 0 "`ntdiv must be 1 or larger"
+    @assert timediv > 0 "`timediv must be 1 or larger"
     @assert length(vars) == length(unique(vars)) == 2 "`vars` must have 2 distinct integer indices"
-    dof = get_numvars()
-    @assert 0 ≤ minimum(vars) && maximum(vars) ≤ dof
+    @assert 0 ≤ minimum(vars) && maximum(vars) ≤ get_numvars()
 
-    return _plot_intvbox(sol, vars=vars, ntdiv=ntdiv)
+    return _plot_intvbox(sol, vars=vars, timediv=timediv)
 end
 
-function _plot_intvbox(sol; vars=(0,1), ntdiv=1)
-    # Initializations
-    normalized_box = symmetric_box(get_numvars(), Float64)
-    tTM  = getfield(sol, :time)
-    xTMv = getfield(sol, :xTMv)
-    plotbox = Vector{IntervalBox{2, Float64}}(undef, ntdiv*length(tTM))
-    v1 = Vector{Interval{Float64}}(undef, length(plotbox))
-    v2 = similar(v1)
+function _plot_intvbox(sol::TMSol; vars=(0,1), timediv=1)
+    domT = mince_in_time(sol, var=0, timediv=timediv)
 
-    # Case in which time is plotted
     if 0 ∈ vars
-        tupT = findfirst(0 ∈ vars)
-        tup2 = vars[3-tupT]
-        ii = 1
-        @inbounds for indT in eachindex(tTM)
-            domTv = mince(domain(xTMv[tup2,indT]), ntdiv)
-            for domT in domTv
-                v1[ii] = tTM[indT]+domT
-                v2[ii] = evaluate(xTMv[tup2,indT](domT), normalized_box)
-                ii += 1
-            end
-        end
-
-        if vars[1] == 0
-            @. plotbox = IntervalBox(v1, v2)
+        tup0 = findfirst(0 ∈ vars)
+        var1 = vars[3-tup0]
+        v1 = _mince_in_time(sol, domT, var1, timediv)
+        if tup0 == 1
+            return @. IntervalBox(domT, v1)
         else
-            @. plotbox = IntervalBox(v2, v1)
+            return @. IntervalBox(v1, domT)
         end
-    else
-
-        # Case in which time is not plotted
-        var1 = vars[1]
-        var2 = vars[2]
-        ii = 1
-        @inbounds for indT in eachindex(tTM)
-            domTv = mince(domain(xTMv[var1,indT]), ntdiv)
-            for domT in domTv
-                v1[ii] = evaluate(xTMv[var1,indT](domT), normalized_box)
-                v2[ii] = evaluate(xTMv[var2,indT](domT), normalized_box)
-                ii += 1
-            end
-        end
-        @. plotbox = IntervalBox(v1, v2)
     end
 
-    return plotbox
+    var1, var2 = vars
+    v1 = _mince_in_time(sol, domT, var1, timediv)
+    v2 = _mince_in_time(sol, domT, var2, timediv)
+    return @. IntervalBox(v1, v2)
+end
+
+function mince_in_time(sol::TMSol; var::Int=0, timediv::Int=0)
+    domT = _mince_in_time(sol, Val(true), timediv)
+    var == 0 && return domT
+    return _mince_in_time(sol, domT, var, timediv)
+end
+
+# Mince in time var (var == 0)
+function _mince_in_time(sol::TMSol, ::Val{true}, timediv::Int=1)
+    tTM  = getfield(sol, :time)
+    xTMv = getfield(sol, :xTMv)
+
+    # Case timediv == 1
+    if timediv == 1
+        return @. tTM + domain.(xTMv[1,:])
+    end
+
+    # Case timediv > 1
+    domT = Vector{typeof(domain(xTMv[1,1]))}(undef, timediv*length(tTM))
+    i0 = 1
+    @inbounds for indT in eachindex(tTM)
+        i1 = indT*timediv
+        domT[i0:i1] .= tTM[indT] .+ mince(domain(xTMv[1,indT]), timediv)
+        i0 = i1 + 1
+    end
+
+    return domT
+end
+
+# Mince other var (var > 0)
+function _mince_in_time(sol::TMSol, domT::Vector{Interval{T}}, var::Int, timediv::Int=1) where {T}
+    N = get_numvars()
+    @assert 1 ≤ var ≤ N
+
+    # Case timediv == 1
+    if timediv == 1
+        fp = getfield(sol, :fp)
+        return @. getindex(getfield(fp, :v), var)
+    end
+
+    # Case timediv > 1
+    vv = similar(domT)
+    normalized_box = symmetric_box(N, Float64)
+    tTM  = getfield(sol, :time)
+    xTMv = getfield(sol, :xTMv)
+    δt = mince(domain(xTMv[1,1]), timediv)
+
+    i0 = 1
+    @inbounds for indT in eachindex(tTM)
+        i1 = indT*timediv
+        δt .= mince(domain(xTMv[1,indT]), timediv)
+        @. vv[i0:i1] = evaluate(evaluate(xTMv[var,indT], δt), (normalized_box,))
+        i0 = i1 + 1
+    end
+
+    return vv    
 end
