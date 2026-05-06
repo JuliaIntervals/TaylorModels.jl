7776edd944bdf5c81c64ebcf1a7fca129250da9d
Luis Benet <lbenet@users.noreply.github.com>
2026-05-06 10:56:37 -0600

    Improvements and additions in methods that deal with the remainders (#194)
    * Move shrink_wrapping! and absorb_remainder to integ_utils.jl
    
    * Fixes in _abs_rems!, and move it to integ_utils.jl
    
    * Add `qrprecondition` and `qrprecondition!` methods
    
    * Add `_evaluate` methods for substituing a vector of TaylorModelN into a TaylorModelN
    
    * Fixes in used functionality of LinearAlgebra and Parameters
    
    * Add _update_inicond!, rename _validation3 to _validation3!, and fixes
    
    * Bump patch version
    
    * Two minor fixes in validated_integ

--- src/valid_integ/ValidatedInteg.jl
+++ src/valid_integ/ValidatedInteg.jl
@@ -5,8 +5,9 @@ module ValidatedInteg
 using TaylorModels
 using Reexport
 @reexport using TaylorIntegration
-using LinearAlgebra: cond, mul!
+using LinearAlgebra: I, cond, mul!, qr
 using Parameters
+import Parameters: @unpack
 using StaticArrays
 
 export TMSol, TMSol3, flowpipe, get_xTM
@@ -21,6 +22,7 @@ const IANumTypes = IA.NumTypes
 
 include("cache.jl")
 include("TMSol.jl")
+include("integ_utils.jl")
 include("validated_integ.jl")
 include("validated_integ2.jl")
 include("validated_integ3.jl")
